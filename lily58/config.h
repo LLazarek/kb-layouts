a85c581dd596f238c0b18a16bd26f288a0d6d1a7
Lukas Lazarek <LukasALazarek@gmail.com>
2024-04-10 11:50:08 -0500

    Adjustments to lily keymap

--- lily58/config.h
+++ lily58/config.h
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define QUICK_TAP_TERM 200
 #define TAPPING_TERM 200
+#define ONESHOT_TAP_TOGGLE 2
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
