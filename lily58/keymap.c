0a47266ec87173c97b62b4dc46dd772dd7dabb1a
Lukas Lazarek <LukasALazarek@gmail.com>
2024-05-31 14:07:24 -0500

    lily keymap updates

--- lily58/keymap.c
+++ lily58/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   LT(_ARROW, KC_W),    KC_E,    LT(_QWERTY, KC_R),    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BACKSLASH,
   KC_LCTL,  LT(_i3, KC_A),   LGUI_T(KC_S),    LALT_T(KC_D),    LCTL_T(KC_F),    KC_G,                     KC_H,    RCTL_T(KC_J),    RALT_T(KC_K),    RGUI_T(KC_L),    KC_SCLN, KC_QUOT,
-  OSM(MOD_LSFT),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    LT(_QWERTY, KC_COMM), LT(_QWERTY, KC_DOT),  KC_SLSH,  OSM(MOD_LSFT),
+  OSM(MOD_LSFT),  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    LT(_QWERTY, KC_COMM), LT(_QWERTY, KC_DOT),  KC_SLSH,  KC_LEFT_GUI,
                         OSL(_NUM), OSL(_NUM), KC_LPRN, KC_ENT, KC_SPC, KC_MINS, KC_BSPC, KC_ESC
 ),
 
 [_NUM] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, _______, _______, _______, _______, _______,                   KC_ASTR, KC_7, KC_8, KC_9, _______, KC_F12,
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_PLUS, KC_4, KC_5, KC_6, KC_EQUAL, KC_LBRC,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MINUS, KC_1, KC_2, KC_3, KC_SLSH, KC_RCBR,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+  _______, KC_F11, KC_F12, _______, _______, _______,                   KC_ASTR, KC_7, KC_8, KC_9, _______, _______,
+  _______, _______, _______, _______, _______, _______,                    KC_PLUS, KC_4, KC_5, KC_6, KC_EQUAL, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MINUS, KC_1, KC_2, KC_3, KC_SLSH, _______,
                              _______, _______, _______, _______, _______,  KC_0, KC_DOT, _______
 ),
 
